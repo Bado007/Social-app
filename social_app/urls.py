dd704614ac4377565b7866e0fd02103a706b7628
Blaz <blaz.kosica@gmail.com>
2022-11-30 17:38:42 +0000

    add logic to upload view so now we can uupload images and be redirected to the same page, make changes in index.html

--- social_app/urls.py
+++ social_app/urls.py
@@ -6,6 +6,7 @@ from social_app import views
 urlpatterns = [
     path('', views.index, name='index.html'),
     path('settings', views.settings, name='setting.html'),
+    path('upload', views.upload, name='upload'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout')
