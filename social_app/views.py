a48c08ae478a5db142702e46706fa30d3d5bccf0
Blaz <blaz.kosica@gmail.com>
2022-12-01 14:56:06 +0000

    add like function

--- social_app/views.py
+++ social_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 # Create your views here.
@@ -65,7 +65,25 @@ def settings(request):
 
 @login_required(login_url='signin')
 def like_post(request):
-    pass
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    # Filter if user has already liked the post => if doesnt work use 'get' instead
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
 
 
 def signup(request):
