7c98434948a2f4a4cdc97e41a126b6d2c0655fb5
Blaz <blaz.kosica@gmail.com>
2022-12-02 01:51:26 +0000

    add follow and unfollow => model, view, url, add changes to 'profile.html'

--- social_app/views.py
+++ social_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 
 
 # Create your views here.
@@ -58,7 +58,7 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-        return redirect('settings')
+        return redirect('setting.html')
 
     return render(request, 'setting.html', {'user_profile': user_profile})
 
@@ -93,15 +93,45 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
+        'user_followers': user_followers,
+        'user_following': user_following,
         }
 
     return render(request, 'profile.html', context)
 
+
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect('/')
 
 
 def signup(request):
@@ -123,11 +153,11 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                #log user in and redirect to settings page
+                # log user in and redirect to settings page
                 user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
 
-                #create a Profile object for the new user
+                # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
@@ -135,13 +165,13 @@ def signup(request):
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
-        
+
     else:
         return render(request, 'signup.html')
 
 
 def signin(request):
-    
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
